5f6f147ef2ec04db1391ee46f42b242a12ada862
Hakobyan0001 <hakobyan.xcho@gmail.com>
2023-09-21 18:09:09 +0400

    create Login and Registration pages

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import {
+  Grid,
+  Paper,
+  Avatar,
+  TextField,
+  Button,
+  Typography,
+  Link,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+
+function Login() {
+  const paperStyle = {
+    padding: 20,
+    width: 280,
+    margin: "20px auto",
+  };
+  const avatarStyle = { backgroundColor: "#1bbd7e" };
+  const btnstyle = { margin: "8px 0" };
+  const textfieldStyle = { margin: "5px 0" };
+
+  return (
+    <Grid>
+      <Paper elevation={10} style={paperStyle}>
+        <Grid align="center">
+          <Avatar style={avatarStyle}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <h2>Sign In</h2>
+        </Grid>
+        <TextField
+          style={textfieldStyle}
+          label="Username"
+          placeholder="Enter username"
+          fullWidth
+          required
+        />
+        <TextField
+          style={textfieldStyle}
+          label="Password"
+          placeholder="Enter password"
+          type="password"
+          fullWidth
+          required
+        />
+        <FormControlLabel
+          control={<Checkbox name="checkedB" color="primary" />}
+          label="Remember me"
+        />
+        <Button
+          type="submit"
+          color="primary"
+          variant="contained"
+          style={btnstyle}
+          fullWidth
+        >
+          Sign in
+        </Button>
+        <Typography>
+          <Link href="#">Forgot password ?</Link>
+        </Typography>
+        <Typography>
+          <Link href="#">Create Account</Link>
+        </Typography>
+      </Paper>
+    </Grid>
+  );
+}
+
+export default Login;
