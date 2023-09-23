074e4700699b0507944dd7ed93570c46ec28f221
Hakobyan0001 <hakobyan.xcho@gmail.com>
2023-09-23 17:22:39 +0400

    updated code for home,login and registration pages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./app/App";
+import ReactDOM from "react-dom";
+import { ThemeProvider, createTheme } from "@mui/material";
+import App from "./app/App"; // Make sure to import your App component or the root component here
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const theme = createTheme();
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Grid,
   Paper,
@@ -13,6 +13,7 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 
 function Login() {
+  const [userInfo, setUserInfo] = useState({ uName: "", pass: "" });
   const paperStyle = {
     padding: 20,
     width: 280,
@@ -22,6 +23,18 @@ function Login() {
   const btnstyle = { margin: "8px 0" };
   const textfieldStyle = { margin: "5px 0" };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(userInfo);
+    setUserInfo({ uName: "", pass: "" });
+  };
+
+  const handleChange = (e) => {
+    setUserInfo((prevValue) => ({
+      ...prevValue,
+      [e.target.name]: e.target.value,
+    }));
+  };
   return (
     <Grid>
       <Paper elevation={10} style={paperStyle}>
@@ -31,34 +44,42 @@ function Login() {
           </Avatar>
           <h2>Sign In</h2>
         </Grid>
-        <TextField
-          style={textfieldStyle}
-          label="Username"
-          placeholder="Enter username"
-          fullWidth
-          required
-        />
-        <TextField
-          style={textfieldStyle}
-          label="Password"
-          placeholder="Enter password"
-          type="password"
-          fullWidth
-          required
-        />
-        <FormControlLabel
-          control={<Checkbox name="checkedB" color="primary" />}
-          label="Remember me"
-        />
-        <Button
-          type="submit"
-          color="primary"
-          variant="contained"
-          style={btnstyle}
-          fullWidth
-        >
-          Sign in
-        </Button>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            style={textfieldStyle}
+            name="uName"
+            label="Username"
+            placeholder="Enter username"
+            value={userInfo.uName}
+            onChange={handleChange}
+            fullWidth
+            required
+          />
+          <TextField
+            style={textfieldStyle}
+            name="pass"
+            label="Password"
+            placeholder="Enter password"
+            type="password"
+            value={userInfo.pass}
+            onChange={handleChange}
+            fullWidth
+            required
+          />
+          <FormControlLabel
+            control={<Checkbox name="checkedB" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            color="primary"
+            variant="contained"
+            style={btnstyle}
+            fullWidth
+          >
+            Sign in
+          </Button>
+        </form>
         <Typography>
           <Link href="#">Forgot password ?</Link>
         </Typography>
